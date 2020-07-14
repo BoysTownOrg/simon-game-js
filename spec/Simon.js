6e8ba0d6c9a83d560470210cdd9e0d22dee98424
Seth Bashford <seth.bashford@boystown.org>
2020-07-14 16:37:58 -0500

    add test

--- spec/Simon.js
+++ spec/Simon.js
@@ -2,6 +2,13 @@ import { Simon } from '../lib/Simon.js'
 import { Color } from '../lib/Color.js'
 
 class PresenterStub {
+    clearState() {
+        this.redLitUp_ = false;
+        this.yellowLitUp_ = false;
+        this.greenLitUp_ = false;
+        this.blueLitUp_ = false;
+    }
+
     redLitUp() { return this.redLitUp_; }
 
     greenLitUp() { return this.greenLitUp_; }
@@ -20,6 +27,13 @@ class PresenterStub {
 }
 
 class AudioPlayerStub {
+    clearState() {
+        this.redPlayed_ = false;
+        this.yellowPlayed_ = false;
+        this.greenPlayed_ = false;
+        this.bluePlayed_ = false;
+    }
+
     redPlayed() { return this.redPlayed_; }
 
     greenPlayed() { return this.greenPlayed_; }
@@ -122,6 +136,30 @@ function setScheduleNotifactionTimeMilliseconds(simon, x) {
     simon.setScheduleNotifactionTimeMilliseconds(x);
 }
 
+function clearPresenterState(presenter) {
+    presenter.clearState();
+}
+
+function clearAudioPlayerState(audioPlayer) {
+    audioPlayer.clearState();
+}
+
+function enterYellow(simon) {
+    simon.enterYellow();
+}
+
+function enterRed(simon) {
+    simon.enterRed();
+}
+
+function enterGreen(simon) {
+    simon.enterGreen();
+}
+
+function enterBlue(simon) {
+    simon.enterBlue();
+}
+
 describe("Simon", function () {
     beforeEach(function () {
         this.presenter = new PresenterStub();
@@ -168,5 +206,23 @@ describe("Simon", function () {
         say(this.simon, [Color.red, Color.green]);
         callback(this.timer);
         expectScheduledNotificationsToEqual(this.timer, 1);
+    });
+
+    it("should light up and play entered colors that are correct", function () {
+        say(this.simon, [Color.red, Color.green, Color.blue, Color.yellow]);
+        clearPresenterState(this.presenter);
+        clearAudioPlayerState(this.audioPlayer);
+        enterRed(this.simon);
+        expectTrue(redLitUp(this.presenter));
+        expectTrue(redPlayed(this.audioPlayer));
+        enterGreen(this.simon);
+        expectTrue(greenLitUp(this.presenter));
+        expectTrue(greenPlayed(this.audioPlayer));
+        enterBlue(this.simon);
+        expectTrue(blueLitUp(this.presenter));
+        expectTrue(bluePlayed(this.audioPlayer));
+        enterYellow(this.simon);
+        expectTrue(yellowLitUp(this.presenter));
+        expectTrue(yellowPlayed(this.audioPlayer));
     });
 });
