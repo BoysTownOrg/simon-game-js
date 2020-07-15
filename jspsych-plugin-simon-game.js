b63e8146d5e31bb5074b4f17a7e6f25ec8701e06
Seth Bashford <seth.bashford@boystown.org>
2020-07-15 17:05:08 -0500

    add to main

--- jspsych-plugin-simon-game.js
+++ jspsych-plugin-simon-game.js
@@ -1,46 +1,78 @@
-const pluginName = 'simon-game';
+const pluginName = "simon-game";
 jsPsych.plugins[pluginName] = (function () {
-    var plugin = {};
-    plugin.info = {
-        name: pluginName,
-        description: '',
-        parameters: {
-        }
-    }
-    plugin.trial = function (display_element, trial) {
-        class CognitionScreen {
-            constructor(display_element) {
-                this.greenButton = document.createElement('div');
-                this.redButton = document.createElement('div');
-                this.blueButton = document.createElement('div');
-                this.yellowButton = document.createElement('div');
-                this.doneButton = document.createElement('div');
-                display_element.append(this.greenButton);
-                display_element.append(this.redButton);
-                display_element.append(this.blueButton);
-                display_element.append(this.yellowButton);
-                display_element.append(this.doneButton);
-                this.greenButton.addEventListener('click', function (e) {
-                    this.listener.notifyThatGreenWasClicked();
-                });
-                this.redButton.addEventListener('click', function (e) {
-                    this.listener.notifyThatRedWasClicked();
-                });
-                this.blueButton.addEventListener('click', function (e) {
-                    this.listener.notifyThatBlueWasClicked();
-                });
-                this.yellowButton.addEventListener('click', function (e) {
-                    this.listener.notifyThatYellowWasClicked();
-                });
-                this.doneButton.addEventListener('click', function (e) {
-                    this.listener.notifyThatDoneWasClicked();
-                });
-            }
+  var plugin = {};
+  plugin.info = {
+    name: pluginName,
+    description: "",
+    parameters: {},
+  };
+  plugin.trial = function (display_element, trial) {
+    class CognitionScreen {
+      constructor(display_element) {
+        this.greenButton = document.createElement("div");
+        this.redButton = document.createElement("div");
+        this.blueButton = document.createElement("div");
+        this.yellowButton = document.createElement("div");
+        this.doneButton = document.createElement("div");
+        display_element.append(this.greenButton);
+        display_element.append(this.redButton);
+        display_element.append(this.blueButton);
+        display_element.append(this.yellowButton);
+        display_element.append(this.doneButton);
+        this.greenButton.addEventListener("click", function (e) {
+          this.listener.notifyThatGreenWasClicked();
+        });
+        this.redButton.addEventListener("click", function (e) {
+          this.listener.notifyThatRedWasClicked();
+        });
+        this.blueButton.addEventListener("click", function (e) {
+          this.listener.notifyThatBlueWasClicked();
+        });
+        this.yellowButton.addEventListener("click", function (e) {
+          this.listener.notifyThatYellowWasClicked();
+        });
+        this.doneButton.addEventListener("click", function (e) {
+          this.listener.notifyThatDoneWasClicked();
+        });
+      }
 
-            subscribe(e) { this.listener = e; }
-        }
-        let screen = new CognitionScreen(display_element);
-        jsPsych.finishTrial();
-    };
-    return plugin;
+      subscribe(e) {
+        this.listener = e;
+      }
+    }
+
+    class WebAudioContext {
+      constructor() {
+        const AudioContext = window.AudioContext || window.webkitAudioContext;
+        this.audioContext = new AudioContext();
+      }
+
+      currentTimeSeconds() {
+        return this.audioContext.currentTime;
+      }
+
+      createToneGenerator() {
+        return new WebAudioOscillator(this.audioContext);
+      }
+    }
+
+    class WebAudioOscillator {
+      constructor(audioContext) {
+        this.oscillator = audioContext.createOscillator();
+      }
+
+      startPlayingAndStopAtSeconds(start, stop) {
+        this.oscillator.start(start);
+        this.oscillator.stop(stop);
+      }
+
+      setFrequencyHz(f) {
+        this.oscillator.frequency.value = f;
+      }
+    }
+
+    let screen = new CognitionScreen(display_element);
+    jsPsych.finishTrial();
+  };
+  return plugin;
 })();
