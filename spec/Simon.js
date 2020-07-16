20c57db123e07cd8165d819d17d6ec86c4a21131
Seth Bashford <seth.bashford@boystown.org>
2020-07-16 16:16:30 -0500

    refactor

--- spec/Simon.js
+++ spec/Simon.js
@@ -105,39 +105,8 @@ class AudioPlayerStub {
   }
 }
 
-class TimerStub {
-  constructor() {
-    this.scheduledNotifications_ = 0;
-  }
-
-  callback() {
-    this.listener.notify();
-  }
-
-  subscribe(e) {
-    this.listener = e;
-  }
-
-  scheduledNotificationTimeMilliseconds() {
-    return this.scheduledNotificationTimeMilliseconds_;
-  }
-
-  scheduleNotificationAfterMilliseconds(x) {
-    ++this.scheduledNotifications_;
-    this.scheduledNotificationTimeMilliseconds_ = x;
-  }
-
-  scheduledNotifications() {
-    return this.scheduledNotifications_;
-  }
-}
-
 function say(simon, colors) {
   simon.say(colors);
-}
-
-function callback(timer) {
-  timer.callback();
 }
 
 function redLitUp(presenter) {
@@ -174,26 +143,6 @@ function yellowPlayed(audioPlayer) {
 
 function expectTrue(b) {
   expect(b).toBeTrue();
-}
-
-function scheduledNotificationTimeMilliseconds(timer) {
-  return timer.scheduledNotificationTimeMilliseconds();
-}
-
-function scheduledNotifications(timer) {
-  return timer.scheduledNotifications();
-}
-
-function expectScheduledNotificationTimeMillisecondsToEqual(timer, x) {
-  expect(scheduledNotificationTimeMilliseconds(timer)).toEqual(x);
-}
-
-function expectScheduledNotificationsToEqual(timer, n) {
-  expect(scheduledNotifications(timer)).toEqual(n);
-}
-
-function setScheduleNotifactionTimeMilliseconds(simon, x) {
-  simon.setScheduleNotifactionTimeMilliseconds(x);
 }
 
 function clearPresenterState(presenter) {
@@ -248,8 +197,7 @@ describe("Simon", function () {
   beforeEach(function () {
     this.presenter = new PresenterStub();
     this.audioPlayer = new AudioPlayerStub();
-    this.timer = new TimerStub();
-    this.simon = new Simon(this.presenter, this.audioPlayer, this.timer);
+    this.simon = new Simon(this.presenter, this.audioPlayer);
   });
 
   it("should play the color tones on say", function () {
@@ -264,46 +212,6 @@ describe("Simon", function () {
       Color.blue,
       Color.yellow,
     ]);
-  });
-
-  it("should light each color while playing corresponding tone", function () {
-    say(this.simon, [Color.red, Color.green, Color.blue, Color.yellow]);
-    expectTrue(redLitUp(this.presenter));
-    expectTrue(redPlayed(this.audioPlayer));
-    callback(this.timer);
-    expectTrue(greenLitUp(this.presenter));
-    expectTrue(greenPlayed(this.audioPlayer));
-    callback(this.timer);
-    expectTrue(blueLitUp(this.presenter));
-    expectTrue(bluePlayed(this.audioPlayer));
-    callback(this.timer);
-    expectTrue(yellowLitUp(this.presenter));
-    expectTrue(yellowPlayed(this.audioPlayer));
-  });
-
-  it("should schedule timed notification on say", function () {
-    setScheduleNotifactionTimeMilliseconds(this.simon, 1);
-    say(this.simon, [Color.red, Color.green, Color.blue, Color.yellow]);
-    expectScheduledNotificationTimeMillisecondsToEqual(this.timer, 1);
-  });
-
-  it("should schedule timed notification on notify", function () {
-    setScheduleNotifactionTimeMilliseconds(this.simon, 1);
-    say(this.simon, [Color.red, Color.green, Color.blue, Color.yellow]);
-    callback(this.timer);
-    expectScheduledNotificationTimeMillisecondsToEqual(this.timer, 1);
-    expectScheduledNotificationsToEqual(this.timer, 2);
-  });
-
-  it("should only schedule timed notification on say when more than one color", function () {
-    say(this.simon, [Color.red]);
-    expectScheduledNotificationsToEqual(this.timer, 0);
-  });
-
-  it("should only schedule timed notification on notify when more than one color remaining", function () {
-    say(this.simon, [Color.red, Color.green]);
-    callback(this.timer);
-    expectScheduledNotificationsToEqual(this.timer, 1);
   });
 
   it("should light up and play entered colors that are correct", function () {
