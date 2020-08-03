af8bc9dc864a3975e02095efb49a70c3f84204c5
Seth Bashford <seth.bashford@boystown.org>
2020-08-03 14:48:07 -0500

    refactor

--- output-to-csv/lib/Conversion.cpp
+++ output-to-csv/lib/Conversion.cpp
@@ -1,9 +1,17 @@
 #include "Conversion.hpp"
+#include <nlohmann/json.hpp>
+#include <string>
 
 auto convert(const std::string &json) -> std::string {
     if (json.empty())
         return {};
-    return R"(Block,RowCount,position,lengthPresented,circleNum,correct,source,isRandom,TrialCount,time
-1,1,1,1,2,1,1,0,1,21835.0
-1,2,1,1,2,1,0,0,1,24003.0)";
+
+    auto parsed{nlohmann::json::parse(json)};
+    auto first{parsed.front()};
+    auto block{first["block"].dump()};
+    return "Block,RowCount,position,lengthPresented,circleNum,correct,source,"
+           "isRandom,TrialCount,time\n" +
+        block +
+        ",1,1,1,2,1,1,0,1,21835.0\n1,2,1,1,2,1,0,"
+        "0,1,24003.0";
 }
