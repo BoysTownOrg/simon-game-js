2ec97f8347c22bf94d3703bcef604245e04a1aec
Seth Bashford <seth.bashford@boystown.org>
2020-08-03 14:57:56 -0500

    beautiful

--- output-to-csv/lib/Conversion.cpp
+++ output-to-csv/lib/Conversion.cpp
@@ -11,10 +11,13 @@ auto convert(const std::string &json) -> std::string {
     auto block{first["block"].dump()};
     auto correct{first["correct"].dump()};
     auto firstSimonMilliseconds{first["simon"].front()["milliseconds"].dump()};
+    auto firstResponseMilliseconds{
+        first["responses"].front()["milliseconds"].dump()};
     return "Block,RowCount,position,lengthPresented,circleNum,correct,source,"
            "isRandom,TrialCount,time\n" +
         block + ",1,1,1,2," + std::to_string(correct == "true" ? 1 : 0) +
         ",1,0,1," + firstSimonMilliseconds + ".0" +
         "\n1,2,1,1,2,1,0,"
-        "0,1,24003.0";
+        "0,1," +
+        firstResponseMilliseconds + ".0";
 }
