dcbb9bb1f3dd58f527f307567a64bf395db0458f
Seth Bashford <seth.bashford@boystown.org>
2020-08-03 15:17:12 -0500

    better

--- output-to-csv/lib/Conversion.cpp
+++ output-to-csv/lib/Conversion.cpp
@@ -10,23 +10,38 @@ static auto formattedMilliseconds(const std::string &s) -> std::string {
     return s + ".0";
 }
 
+static void addValueWithComma(std::string &s, const std::string &value) {
+    s += value + ',';
+}
+
 auto convert(const std::string &json) -> std::string {
     if (json.empty())
         return {};
 
     auto parsed{nlohmann::json::parse(json)};
-    auto first{parsed.front()};
-    auto block{first["block"].dump()};
-    auto correct{first["correct"].dump()};
-    auto firstSimonMilliseconds{first["simon"].front()["milliseconds"].dump()};
+    auto firstTrial{parsed.front()};
+    auto block{firstTrial["block"].dump()};
+    auto correct{firstTrial["correct"].dump()};
+    auto firstSimonMilliseconds{
+        firstTrial["simon"].front()["milliseconds"].dump()};
     auto firstResponseMilliseconds{
-        first["responses"].front()["milliseconds"].dump()};
-    auto isRandom{first["isRandom"].dump()};
-    return "Block,RowCount,position,lengthPresented,circleNum,correct,source,"
-           "isRandom,TrialCount,time\n" +
-        block + ",1,1,1,2," + booleanStringToIntegerString(correct) +
-        ",1,0,1," + formattedMilliseconds(firstSimonMilliseconds) + "\n" +
-        block + ",2,1,1,2," + booleanStringToIntegerString(correct) + ",0," +
+        firstTrial["responses"].front()["milliseconds"].dump()};
+    auto isRandom{firstTrial["isRandom"].dump()};
+    std::string conversion;
+    addValueWithComma(conversion, "Block");
+    addValueWithComma(conversion, "RowCount");
+    addValueWithComma(conversion, "position");
+    addValueWithComma(conversion, "lengthPresented");
+    addValueWithComma(conversion, "circleNum");
+    addValueWithComma(conversion, "correct");
+    addValueWithComma(conversion, "source");
+    addValueWithComma(conversion, "isRandom");
+    addValueWithComma(conversion, "TrialCount");
+    conversion += "time\n" + block + ",1,1,1,2," +
+        booleanStringToIntegerString(correct) + ",1,0,1," +
+        formattedMilliseconds(firstSimonMilliseconds) + "\n" + block +
+        ",2,1,1,2," + booleanStringToIntegerString(correct) + ",0," +
         booleanStringToIntegerString(isRandom) + ",1," +
         formattedMilliseconds(firstResponseMilliseconds);
+    return conversion;
 }
