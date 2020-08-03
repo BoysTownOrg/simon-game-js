6d3603ef9d6f520fb9f08b7c0d2d42388f2322db
Seth Bashford <seth.bashford@boystown.org>
2020-08-03 15:00:52 -0500

    more

--- output-to-csv/lib/Conversion.cpp
+++ output-to-csv/lib/Conversion.cpp
@@ -13,11 +13,11 @@ auto convert(const std::string &json) -> std::string {
     auto firstSimonMilliseconds{first["simon"].front()["milliseconds"].dump()};
     auto firstResponseMilliseconds{
         first["responses"].front()["milliseconds"].dump()};
+    auto isRandom{first["isRandom"].dump()};
     return "Block,RowCount,position,lengthPresented,circleNum,correct,source,"
            "isRandom,TrialCount,time\n" +
-        block + ",1,1,1,2," + std::to_string(correct == "true" ? 1 : 0) +
-        ",1,0,1," + firstSimonMilliseconds + ".0" +
-        "\n1,2,1,1,2,1,0,"
-        "0,1," +
+        block + ",1,1,1,2," + std::string{correct == "true" ? '1' : '0'} +
+        ",1,0,1," + firstSimonMilliseconds + ".0" + "\n1,2,1,1,2,1,0," +
+        std::string{isRandom == "true" ? '1' : '0'} + ",1," +
         firstResponseMilliseconds + ".0";
 }
