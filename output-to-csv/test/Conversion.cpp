ed7b3169bd07e79792d59fe5471261d2fc8d4eba
Seth Bashford <seth.bashford@boystown.org>
2020-08-04 09:31:16 -0500

    shaking test up a little

--- output-to-csv/test/Conversion.cpp
+++ output-to-csv/test/Conversion.cpp
@@ -20,13 +20,13 @@ void oneTrialWithOneSimonToneAndOneResponseYieldsTwoRows(
 1,2,1,1,2,1,0,0,1,24003.0)",
         R"([
   {
-    "correct": true,
     "responses": [{ "milliseconds": 24003, "id": 2 }],
-    "simon": [{ "milliseconds": 21835, "id": 2 }],
+    "correct": true,
     "color_key": { "red": 1, "green": 3, "yellow": 2, "blue": 4 },
+    "simon": [{ "milliseconds": 21835, "id": 2 }],
     "color_locations": { "top": 1, "left": 3, "right": 4, "bottom": 2 },
-    "block": 1,
     "isRandom": false,
+    "block": 1,
     "trial_type": "simon-game-colored-circles",
     "trial_index": 1,
     "time_elapsed": 50000,
@@ -44,18 +44,18 @@ void oneTrialWithTwoSimonTonesAndOneResponseYieldsThreeRows(
 1,3,1,2,4,0,0,1,1,789.0)",
         R"([
   {
-    "correct": false,
     "responses": [{ "milliseconds": 789, "id": 4 }],
     "simon": [
       { "milliseconds": 123, "id": 3 },
       { "milliseconds": 456, "id": 2 }
     ],
     "color_key": { "red": 1, "green": 3, "yellow": 2, "blue": 4 },
-    "color_locations": { "top": 1, "left": 3, "right": 4, "bottom": 2 },
     "block": 1,
-    "isRandom": true,
+    "color_locations": { "top": 1, "left": 3, "right": 4, "bottom": 2 },
     "trial_type": "simon-game-colored-circles",
+    "isRandom": true,
     "trial_index": 1,
+    "correct": false,
     "time_elapsed": 1000,
     "internal_node_id": "0.0-1.0-0.0"
   }
@@ -77,17 +77,17 @@ void oneTrialWithTwoSimonTonesAndTwoResponsesYieldsFourRows(
       { "milliseconds": 789, "id": 4 },
       { "milliseconds": 1010, "id": 1 }
     ],
+    "time_elapsed": 1000,
+    "color_key": { "red": 1, "green": 3, "yellow": 2, "blue": 4 },
+    "color_locations": { "top": 1, "left": 3, "right": 4, "bottom": 2 },
+    "isRandom": true,
+    "block": 1,
+    "trial_type": "simon-game-colored-circles",
+    "trial_index": 1,
     "simon": [
       { "milliseconds": 123, "id": 3 },
       { "milliseconds": 456, "id": 2 }
     ],
-    "color_key": { "red": 1, "green": 3, "yellow": 2, "blue": 4 },
-    "color_locations": { "top": 1, "left": 3, "right": 4, "bottom": 2 },
-    "block": 1,
-    "isRandom": true,
-    "trial_type": "simon-game-colored-circles",
-    "trial_index": 1,
-    "time_elapsed": 1000,
     "internal_node_id": "0.0-1.0-0.0"
   }
 ])");
