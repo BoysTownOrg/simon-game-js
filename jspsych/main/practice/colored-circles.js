cf4b9b3e45182f4e2535eea1bbb99a5aced5db53
Seth Bashford <seth.bashford@boystown.org>
2020-11-12 16:17:51 -0600

    refactor

--- jspsych/main/practice/colored-circles.js
+++ jspsych/main/practice/colored-circles.js
@@ -2,27 +2,12 @@ import * as simon from "../../../lib/index.js";
 import * as simonJsPsychPlugins from "../../plugin.js";
 import * as jsPsychUtility from "../../utility.js";
 
-// https://stackoverflow.com/a/2450976
-function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
-}
-
 function colorOrder(orderedColors, n) {
   return [orderedColors.get(n), n];
 }
 
 // https://stackoverflow.com/a/10050831
-const order = shuffle([...Array(4).keys()]);
+const order = jsPsychUtility.shuffle([...Array(4).keys()]);
 const orderedColors = new Map([
   [order[0], simon.Color.red],
   [order[1], simon.Color.green],
--- jspsych/utility.js
+++ jspsych/utility.js
@@ -1,5 +1,20 @@
 import * as simon from "../lib/index.js";
 
+// https://stackoverflow.com/a/2450976
+export function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
 export function arrayToHtml(lines) {
   let html = "";
   for (const line of lines) {
