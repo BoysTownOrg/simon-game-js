b697fac9c4d7adfcc3c738c23617eb3310e7c681
Seth Bashford <seth.bashford@boystown.org>
2020-11-17 10:34:02 -0600

    same

--- jspsych/main/colored-circles-with-button-advancing-screen.js
+++ jspsych/main/colored-circles-with-button-advancing-screen.js
@@ -9,10 +9,10 @@ jsPsych.plugins[simonPluginId] = simonJsPsychPlugins.coloredCircles(
 const timeline = [];
 jsPsychUtility.pushParticipantIdForm(timeline);
 jsPsychUtility.pushContinueButtonResponse(timeline, [
-  "You will see patterns of colored circles shown on the screen in different places, one at a time. After watching each pattern, you must correctly copy it by pressing the place/color where you saw it.",
-  'When you finish copying each pattern, press the "Done" button and then the next pattern will be shown.',
-  'For example, if you see the pattern BLUE-RED-GREEN, then you should press the colors blue, red, green in that order, and then press "Done" at the bottom.',
-  "If you don't know or can't remember what a pattern was, just make your best guess. Once you make a response, you cannot go back and correct it, so take your time in choosing the correct colors.",
+  coloredCircles.instruction11,
+  coloredCircles.instruction12,
+  coloredCircles.instruction13,
+  coloredCircles.instruction14,
   "Watch me!",
 ]);
 const firstTrial = {
